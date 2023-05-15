a5274385c079669619f933e0591131f5c18348cd
HyoHwan <gyghks0378@gmail.com>
2023-05-16 01:21:01 +0900

    [Feat] 로그인,회원가입

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,14 @@
 import { Button, TextField, colors } from '@mui/material'
-import React from 'react'
+import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { styled, css } from 'styled-components'
-
+import { styled } from 'styled-components'
+import axios from 'axios'
 
 function Login() {
+  const [loginForm, setLoginForm] = useState({
+    userid: '',
+    password: ''
+  })
 
   const navigate = useNavigate()
 
@@ -12,12 +16,42 @@ function Login() {
     navigate('/signup')
   }
 
+  const handleLogin = async () => {
+    try {
+      // 서버로 보낼 데이터
+      const loginData = {
+        userid: loginForm.userid,
+        password: loginForm.password
+      }
+
+      // 서버로 데이터 전송
+      const response = await axios.post('http://13.125.6.183:8080/login', loginData)
+      console.log('데이터 : ', response.data)
+
+    } catch (error) {
+      console.error('로그인 실패:', error)
+    }
+  }
+
+  const handleFormChange = (e) => {
+    const { name, value } = e.target;
+    setLoginForm({ ...loginForm, [name]: value })
+  }
+
+
+
   return (
     <Container>
       <LoginForm>
         <h1>Title</h1>
         <TextFidelContainer>
-          <TextField id="outlined-basic" label="아이디" variant="outlined"
+          <TextField
+            id="userid"
+            name="userid"
+            label="아이디"
+            variant="outlined"
+            value={loginForm.userid}
+            onChange={handleFormChange}
             sx={{
               width: '300px',
               backgroundColor: 'white'
@@ -25,7 +59,14 @@ function Login() {
           />
         </TextFidelContainer>
         <TextFidelContainer>
-          <TextField id="outlined-basic" label="비밀번호" variant="outlined" type='password'
+          <TextField
+            id="password"
+            name="password"
+            label="비밀번호"
+            variant="outlined"
+            type="password"
+            value={loginForm.password}
+            onChange={handleFormChange}
             sx={{
               width: '300px',
               backgroundColor: 'white'
@@ -33,7 +74,9 @@ function Login() {
           />
         </TextFidelContainer>
         <ButtonContainer>
-          <Button variant="contained"
+          <Button
+            variant="contained"
+            onClick={handleLogin}
             sx={{
               backgroundColor: 'white',
               color: 'black',
@@ -42,8 +85,12 @@ function Login() {
                 color: 'white'
               }
             }}
-          >로그인</Button>
-          <Button variant="contained" onClick={handleSignup}
+          >
+            로그인
+          </Button>
+          <Button
+            variant="contained"
+            onClick={handleSignup}
             sx={{
               backgroundColor: 'white',
               color: 'black',
@@ -52,7 +99,9 @@ function Login() {
                 color: 'white'
               }
             }}
-          >회원가입</Button>
+          >
+            회원가입
+          </Button>
         </ButtonContainer>
       </LoginForm>
     </Container>
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,82 +1,221 @@
 import { TextField, Button } from '@mui/material'
 import React, { useState } from 'react'
 import { styled } from 'styled-components'
-import { colors } from '@mui/material'
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
+import { DatePicker } from '@mui/x-date-pickers/DatePicker'
+import { addUsers } from '../api/users'
+import moment from 'moment/moment'
+import { useNavigate } from 'react-router-dom'
+
 
 function Signup() {
-
-  // 이미지 업로드 스코프 시작
   const [imagePreview, setImagePreview] = useState(null)
+  const [userid, setUserId] = useState('')
+  const [username, setUserName] = useState('')
+  const [password, setPassword] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState('')
+  const [userIdError, setUserIdError] = useState('')
+  const [nameError, setNameError] = useState('')
+  const [passwordError, setPasswordError] = useState('')
+  const [confirmPasswordError, setConfirmPasswordError] = useState('')
+  const [selectedDate, setSelectedDate] = useState(null)
+
+  const navigate = useNavigate()
+  // console.log(imagePreview)
+
+  let file
   const ImageUploadHandler = (e) => {
-    const file = e.target.files[0]
+    file = e.target.files[0]
     const reader = new FileReader()
 
     console.log('사진 : ', file)
+
     reader.onloadend = () => {
-      setImagePreview(reader.result)
+      setImagePreview(file) // 이미지 데이터를 그대로 설정
     }
 
     if (file) {
-      reader.readAsDataURL(file)
+      reader.readAsArrayBuffer(file)
     }
   }
-  // 이미지 업로드 스코프 끝
 
   const DuplicateCheck = () => {
-    //중복검사 로직 들어갈 자리
+    // 중복검사 로직
   }
+
+  const SignUpClick = async () => {
+    let isValid = true
+
+    const useridRegex = /^[a-zA-Z0-9]{4,12}$/ // 영어 알파벳 4자리에서 12자리
+    const nameRegex = /^[가-힣]{2,10}$/ // 한글 2 - 10자리
+    const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/ //알파벳 대소문자 하나 이상 숫자중 하나 이상 6자리 이상
+
+    if (!userid) {
+      setUserIdError('아이디를 입력해주세요.')
+      isValid = false
+    } else if (!useridRegex.test(userid)) {
+      setUserIdError('아이디는 알파벳과 숫자로 이루어진 4~12자여야 합니다.')
+      isValid = false
+    } else {
+      setUserIdError('')
+    }
+
+    if (!username) {
+      setNameError('이름을 입력해주세요.')
+      isValid = false
+    } else if (!nameRegex.test(username)) {
+      setNameError('이름은 알파벳으로 이루어진 2~10자여야 합니다.')
+      isValid = false
+    } else {
+      setNameError('')
+    }
+
+    if (!password) {
+      setPasswordError('비밀번호를 입력해주세요.')
+      isValid = false;
+    } else if (!passwordRegex.test(password)) {
+      setPasswordError(
+        '비밀번호는 최소 6자 이상이어야 하며, 영문자와 숫자가 모두 포함되어야 합니다.'
+      )
+      isValid = false
+    } else {
+      setPasswordError('')
+    }
+
+    if (password !== confirmPassword) {
+      setConfirmPasswordError('비밀번호가 일치하지 않습니다.')
+      isValid = false
+    } else {
+      setConfirmPasswordError('')
+    }
+
+    if (isValid) {
+      const formattedDate = moment(selectedDate).format('YYYY-MM-DD')
+      const formData = new FormData()
+      formData.append('userid', userid)
+      formData.append('username', username)
+      formData.append('password', password)
+      formData.append('birthday', formattedDate)
+      formData.append('image', imagePreview, file)
+
+      try {
+        const response = await addUsers(formData)
+        console.log('formdata : ', response)
+        navigate('/')
+
+      } catch (error) {
+        console.error('addUsers Error : ', error)
+      }
+    }
+  }
+
 
   return (
     <Container>
-      <SignUpForm>
-
+      <SignUpForm
+        encType="multipart/form-data"
+        method='post'
+        onSubmit={(e) => {
+          e.preventDefault()
+        }}
+      >
         <FileUploadContainer>
           <ImagePreview src={imagePreview} alt="Preview" />
           <input type="file" accept="image/*" onChange={ImageUploadHandler} />
         </FileUploadContainer>
 
         <IdContainer>
-          <TextField id="standard-basic" label="아이디" variant="standard"
+          <TextField
+            id="userid"
+            label="아이디"
+            variant="standard"
+            value={userid}
+            onChange={(e) => setUserId(e.target.value)}
+            error={!!userIdError}
+            helperText={userIdError}
             sx={{
               width: '235px',
-              margin: '20px'
+              margin: '20px',
             }}
           />
           <Button variant="contained" onClick={DuplicateCheck}
             sx={{
               width: '90px',
-              height: '50px'
-            }}>중복검사</Button>
+              height: '50px',
+            }}
+          >
+            중복검사
+          </Button>
         </IdContainer>
 
-        <TextField id="standard-basic" label="이름" variant="standard"
+        <TextField
+          id="name"
+          label="이름"
+          variant="standard"
+          value={username}
+          onChange={(e) => setUserName(e.target.value)}
+          error={!!nameError}
+          helperText={nameError}
           sx={{
             width: '300px',
-            margin: '20px'
+            margin: '20px',
           }}
         />
-        <TextField id="standard-basic" label="비밀번호" variant="standard" type="password"
+        <TextField
+          id="password"
+          label="비밀번호"
+          variant="standard"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          error={!!passwordError}
+          helperText={passwordError}
           sx={{
             width: '300px',
-            margin: '20px'
+            margin: '20px',
           }}
         />
-        <TextField id="standard-basic" label="비밀번호 확인" variant="standard" type="password"
+        <TextField
+          id="confirm-password"
+          label="비밀번호 확인"
+          variant="standard"
+          type="password"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+          error={!!confirmPasswordError}
+          helperText={confirmPasswordError}
           sx={{
             width: '300px',
-            margin: '20px'
+            margin: '20px',
           }}
         />
-        <Button variant="contained" onClick={DuplicateCheck}
-          sx={{
-            width: '300px',
-            marginTop: '30px'
-          }}>가입하기</Button>
 
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DemoContainer components={['DatePicker']}>
+            <DatePicker
+              label="생년월일"
+              value={selectedDate}
+              onChange={(date) => setSelectedDate(date)}
+            />
+          </DemoContainer>
+        </LocalizationProvider>
+
+
+        <Button variant="contained" onClick={SignUpClick}
+          sx={{
+            width: '300px',
+            marginTop: '30px',
+          }}
+        >
+          가입하기
+        </Button>
       </SignUpForm>
-    </Container >
+    </Container>
   )
 }
+
 
 export default Signup
 
@@ -88,7 +227,7 @@ const Container = styled.div`
   height: 100vh;
 `
 
-const SignUpForm = styled.div`
+const SignUpForm = styled.form`
   display: flex;
   flex-direction: column;
   align-items: center;
