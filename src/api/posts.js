ee868f0ebea9e224b535cf9b840b7b834ec5acb1
HyoHwan <gyghks0378@gmail.com>
2023-05-18 17:20:42 +0900

    [fix] api 수정

--- src/api/posts.js
+++ src/api/posts.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const usersList = async () => {
   try {
-    const response = await axios.get(`${process.env.REACT_APP_SERVER_URL}//users/user-info`)
+    const response = await axios.get(`${process.env.REACT_APP_SERVER_URL}/users/user-info`)
     return response.data;
   } catch (error) {
     throw new Error('Failed to fetch users')
